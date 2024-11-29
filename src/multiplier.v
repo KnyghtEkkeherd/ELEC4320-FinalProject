6822ed8ae0e973a68fa43ce5cd2e4c55dde94942
Wiktor Kowalczyk <wiktormkowalczyk@gmail.com>
2024-11-29 13:34:22 +0800

    multiplier init

---
+++ src/multiplier.v
@@ -0,0 +1,20 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Name: Wiktor Kowalczyk
+// Student ID: 20814029
+// Email: wmkowalczyk@connect.ust.hk
+//////////////////////////////////////////////////////////////////////////////////
+
+module multiplier (
+    input [31:0] A,
+    input [31:0] B,
+    input clk,
+    output [63:0] P
+);
+
+    reg P;
+    always @(posedge clk) begin
+
+    end
+
+endmodule
