682393dffa5ce3d4d2bfca1decab0e3da453df49
Wiktor Kowalczyk <wiktormkowalczyk@gmail.com>
2024-12-12 14:03:40 +0800

    Kogge-Stone 8bit, 16bit, and 32bit working

--- src/adders/kogge_stone_adder32bit.v
+++ src/adders/kogge_stone_adder32bit.v
@@ -5,30 +5,32 @@
 // Email: wmkowalczyk@connect.ust.hk
 //////////////////////////////////////////////////////////////////////////////////
 
-module kogge_stone_adder16bit (
-    input [15:0] A,
-    input [15:0] B,
+`timescale 1ns / 1ps
+
+module kogge_stone_adder32bit (
+    input [31:0] A,
+    input [31:0] B,
     input Cin,
-    output [15:0] S,
+    output [31:0] S,
     output Cout
 );
 
-    wire [7:0] S0, S1;  // Sum outputs from the two 8-bit adders
-    wire Cout0;  // Carry out from the first 8-bit adder
+    wire [15:0] S0, S1;  // Sum outputs from the two 16-bit adders
+    wire Cout0;  // Carry out from the first 16-bit adder
 
-    // Instantiate the first 8-bit Kogge-Stone adder
-    kogge_stone_adder8bit KSA0 (
-        .A(A[7:0]),
-        .B(B[7:0]),
+    // Instantiate the first 16-bit Kogge-Stone adder
+    kogge_stone_adder16bit KSA0 (
+        .A(A[15:0]),
+        .B(B[15:0]),
         .Cin(Cin),
         .S(S0),
         .Cout(Cout0)
     );
 
-    // Instantiate the second 8-bit Kogge-Stone adder
-    kogge_stone_adder8bit KSA1 (
-        .A(A[15:8]),
-        .B(B[15:8]),
+    // Instantiate the second 16-bit Kogge-Stone adder
+    kogge_stone_adder16bit KSA1 (
+        .A(A[31:16]),
+        .B(B[31:16]),
         .Cin(Cout0),  // Carry out from the first adder becomes the carry in for the second
         .S(S1),
         .Cout(Cout)
